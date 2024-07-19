660283f18a2acad70c0f9cbbc9596786454c82cf
bluestar7589 <bluestar7589@yahoo.com>
2024-07-19 11:26:02 -0700

    Added example to select all vendors

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using EntityFramework.Data;
+using EntityFramework.Models;
+
 namespace EntityFramework
 {
     public partial class Form1 : Form
@@ -5,6 +8,24 @@ namespace EntityFramework
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method to get all from vendors table from the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using ApContext context = new ApContext();
+
+            List<Vendor> lstVendorsMethod = new List<Vendor>();
+            // LINQ - Languate Integrated Query - method sybtax
+            lstVendorsMethod = context.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> lstVendorsQuery = new List<Vendor>();
+            lstVendorsQuery = (from v in context.Vendors select v).ToList();
         }
     }
 }
